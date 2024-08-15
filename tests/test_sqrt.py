4fd16dc16375e61501512d72b076d472701b1c8f
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-15 19:59:55 +0200

    Update test_sqrt.py

--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -10,13 +10,10 @@ class TestSqrt(unittest.TestCase):
         """method to test square root outputs"""
         self.assertEqual(sqrt(25), 5, "should be 5")
         self.assertEqual(sqrt(144), 12, "should be 12")
-    
+
     def test_sqrt_negatives(self):
         """method to test square root for negative input"""
         self.assertEqual(sqrt(-12), ValueError, "input should be positive")
-    
-    def test_invalid_inputs(self):
-        self.assertRaises(ValueError, sqrt, ["Hello", "3"])
 
 
 if __name__ == "__main__":
